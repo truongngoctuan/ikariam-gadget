a0875979d1a5f604dd564c5f443ab57a71424d81
hoangminh89 <hoangminh89@aa63638d-d635-bbc2-c20a-bf1a66b5361b>
2011-01-13 09:07:01 +0000

    Xong phần liên kết giữa Javascript - .NET

---
+++ IkariamFramework/Account.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IkariamFramework
+{
+    public class Account
+    {
+        City[] _Cities = null;
+
+        public City[] Cities
+        {
+            get { return _Cities; }
+            set { _Cities = value; }
+        }
+        
+        public static void Login(string strUsername, string strPassword)
+        {
+            BaseFunction.PostGetHtmlSite(GloVal.Document, GloVal.cookieContainer,
+                "http://s15.en.ikariam.com/index.php?action=loginAvatar&function=login",
+                string.Format("uni_url=s15.en.ikariam.com&name={0}&password={1}&kid=", 
+                    strUsername, 
+                    strPassword));
+        }
+    }
+}
---
+++ IkariamFramework/Gadget.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using HtmlAgilityPack;
+using System.Windows.Forms;
+
+namespace IkariamFramework
+{
+    /// <summary>
+    /// A Gateway for receiving player info
+    /// </summary>
+    [ComVisible(true)]
+    public class Gadget : IDisposable
+    {
+        public bool Authenticated { get; private set; }
+        public Gadget()
+        {
+            Authenticated = false;
+        }
+
+        public int Login(string username, string password, string server)
+        {
+            // Login : trả về bool, thành công - thất bại
+            // Hoặc tra về errorCode
+            Account.Login(username, password);
+            Authenticated = true;
+            return 0;
+        }
+
+        public String btadvCities_Click()
+        {
+            HtmlNode node = GloVal.Document.DocumentNode.SelectNodes("html/body/div/div/div[13]/ul/li/a")[0];
+            string strhref = node.GetAttributeValue("href", "err");
+            BaseFunction.GetHtmlSite(GloVal.Document, GloVal.cookieContainer,
+                "http://s15.en.ikariam.com/index.php" + strhref);
+            //cap nhat oldview
+            GloVal.strOldView = strhref.Substring(strhref.IndexOf("oldView=") + 8);
+            return GloVal.strOldView;
+        }
+
+        public string GetErrorMessage(int errMessageCode)
+        {
+            string errorMessage = null;
+            if (errors.TryGetValue(errMessageCode, out errorMessage))
+                return errorMessage;
+            return null;
+        }
+
+        Dictionary<int, string> errors = new Dictionary<int, string>
+        {
+            {0, "No errors"},
+            {1, "Unknown errors"},
+            {2, "Username or password incorrect"},
+            {3, "Connection timeout"},
+            {4, "Username and password can't be empty"}
+        };
+
+        #region IDisposable Members
+
+        public void Dispose()
+        {
+            // Nothing to do here.
+        }
+
+        #endregion
+    }
+}
